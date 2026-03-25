0d76247bed31b5eca0f3c857b09c9d8f4f946fe4
dudasdaily <dudasdaily@gmail.com>
2026-03-25 14:17:48 +0900

    백스페이스 컬링 구현

--- Source/Player/SoftRenderer3D.cpp
+++ Source/Player/SoftRenderer3D.cpp
@@ -57,7 +57,7 @@ void SoftRenderer::LoadScene3D()
 }
 
 // 실습 설정을 위한 변수
-
+bool useBackfaceCulling = false;
 
 // 게임 로직을 담당하는 함수
 void SoftRenderer::Update3D(float InDeltaSeconds)
@@ -82,6 +82,9 @@ void SoftRenderer::Update3D(float InDeltaSeconds)
 
 	// 입력에 따른 카메라 트랜스폼의 변경
 	camera.SetLookAtRotation(playerTransform.GetPosition());
+
+	if (input.IsReleased(InputButton::Space))
+		useBackfaceCulling = !useBackfaceCulling;
 }
 
 // 애니메이션 로직을 담당하는 함수
@@ -131,6 +134,9 @@ void SoftRenderer::Render3D()
 			Vector3 viewPosition = vMatrix * transform.GetPosition();
 			r.PushStatisticText("World: " + transform.GetPosition().ToString());
 			r.PushStatisticText("View : " + viewPosition.ToString());
+
+			std::string cullingStatus = useBackfaceCulling ? "True" : "False";
+			r.PushStatisticText("BackfaceCulling : " + cullingStatus);
 		}
 	}
 }
@@ -184,6 +190,18 @@ void SoftRenderer::DrawTriangle3D(std::vector<Vertex3D>& InVertices, const Linea
 {
 	auto& r = GetRenderer();
 	const GameEngine& g = Get3DGameEngine();
+
+	if (useBackfaceCulling)
+	{
+		Vector3 edge1 = (InVertices[1].Position - InVertices[0].Position).ToVector3();
+		Vector3 edge2 = (InVertices[2].Position - InVertices[0].Position).ToVector3();
+
+		Vector3 norm = edge1.Cross(edge2);
+		Vector3 viewDirection = -Vector3::UnitZ;
+
+		if (norm.Dot(viewDirection) >= 0.f)
+			return;
+	}
 
 	LinearColor finalColor = _WireframeColor;
 	if (InColor != LinearColor::Error)
